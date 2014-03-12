f2a5f6f9b7c1ec7993d9d3365577d743fe4cc57d
aaronhaycraft <ahaycraft@csumb.edu>
2014-03-12 15:15:06 -0700

    More advanced GUI working
    Sample advanced GUI is working. Now going to try to experiment with
    other files

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
+import javax.swing as swing
+import java
+
 def GUI_start():
-  import javax.swing as swing
   window = swing.JFrame("Test",size=(300,300))
   text_field=swing.JTextField(preferredSize=(300, 20))
   text_field.text = "Welcome to test sample!"
@@ -11,5 +13,31 @@ def GUI_start():
   window.pack()
   window.visible=1
   
-def GUI_advanced():
+class VideoMaker(swing.JFrame):
+    def __init__(self):
+      swing.JFrame.__init__(self, title="Video Maker", size=(200,200))
+      self.contentPane.layout=java.awt.FlowLayout()
+      
+      self.field=swing.JTextField(size=(200,60))
+      self.field.text="sample.jpg"
+      self.contentPane.add(self.field)
+      
+      fileView = swing.JButton("Create Video", size=(65,30), actionPerformed=self.checkContents)
+      self.contentPane.add(fileView)
+      
+      setFolder = swing.JButton("Set Folder", size=(65,40), actionPerformed=self.setFolder)
+      self.contentPane.add(setFolder)
+      
+      self.visible=1
+      
+    def checkContents(self,event):
+      if self.field.text.endswith(".jpg"):
+        pic = makePicture(getMediaPath(self.field.text))
+        show(pic)
+      if self.field.text.endswith(".wav"):
+        snd=makeSound(getMediaPath(self.field.text))
+        play(snd)
+    
+    def setFolder(self,event):
+      setMediaPath()
   

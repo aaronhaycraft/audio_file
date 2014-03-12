38965f676c4b764495975a33078ff5b004802d2b
aaronhaycraft <ahaycraft@csumb.edu>
2014-03-12 14:26:36 -0700

    Sample GUI Complete
    Completed sample GUI, moving onto more advanced GUI setup.

--- GUI.py
+++ GUI.py
@@ -1,9 +1,15 @@
-import javax.swing as swing
-
-window = swing.JFrame("Test",size=(300,300))
-text_field=swing.JTextField(preferredSize=(300, 20))
-text_field.text = "Welcome to test sample!"
-window.contentPane.add(text_field)
-button = swing.JButton("View", preferredSize=300, 20))
-window.contentPane.add(button)
-window.visible=1
+def GUI_start():
+  import javax.swing as swing
+  window = swing.JFrame("Test",size=(300,300))
+  text_field=swing.JTextField(preferredSize=(300, 20))
+  text_field.text = "Welcome to test sample!"
+  window.contentPane.add(text_field)
+  window.visible=1
+  button = swing.JButton("View", preferredSize=(300, 20))
+  window.contentPane.add(button)
+  window.visible=1
+  window.pack()
+  window.visible=1
+  
+def GUI_advanced():
+  
